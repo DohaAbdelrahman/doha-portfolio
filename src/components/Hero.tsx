a4d3c71fc9c21b5cb46530815b93c7eb6e9fcb8b
DohaAbdelrahman <dohaabdo757@gmail.com>
2025-07-06 17:33:59 +0000

    535

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
               Doha Abdelrahman
             </h1>
 
-            <p className="text-xl md:text-3xl font-semibold mb-6 bg-gradient-primary bg-clip-text text-transparent animate-glow">
+            <p className="text-2xl md:text-4غ8غxl font-semibold mb-6 bg-gradient-primary bg-clip-text text-transparent animate-glow">
               <TypeAnimation
                 sequence={[
                   'Data Scientist',
