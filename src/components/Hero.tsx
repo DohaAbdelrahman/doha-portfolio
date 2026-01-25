a65184bb2ddc5b9a2ae78dceb4825975e4c80b18
Doha Abdelrahman <dohaabdo757@gmail.com>
2026-01-25 01:09:23 +0000

    change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -70,7 +70,7 @@ export const Hero = () => {
                 </Button>
               </a>
               <Button variant="outline" className="border-primary/30 hover:bg-primary/10 text-base font-semibold" onClick={() => {
-              const cvUrl = "https://drive.google.com/file/d/1wRqMd4AVzrC1xSKBJpHl-6r1KJe836d_/view?usp=sharing";
+              const cvUrl =  "https://drive.google.com/uc?export=download&id=1wRqMd4AVzrC1xSKBJpHl-6r1KJe836d_";
               const link = document.createElement('a');
               link.href = cvUrl;
               link.download = 'Doha_Abdelrahman_CV.pdf';
